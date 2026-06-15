df555b8af853937dfc93d32ea0d362ba49e5dd87
Deb <56758812+Deb84@users.noreply.github.com>
2026-06-15 02:51:00 +0000

    Refactor workspace management and event handling

--- miaout/workspace.lua
+++ miaout/workspace.lua
@@ -1,51 +1,111 @@
 local Instance = {}
 Instance.__index = Instance
 
+local function subscribleEvents(self)
+    local hl = self.hl
+
+
+    -- the behaviour is weird here, active workspace is actually the last workspace
+    hl.on("monitor.focused", function (monitor)
+        local lastWorkspace = hl.get_active_workspace() -- dont ask me how but it works
+        self:SaveWorkspaceState(lastWorkspace)
+
+        local workspace = monitor.active_workspace
+
+        self:LoadWorkspaceLayout(workspace)
+    end)
+end
+
 function Instance.new(hl, layout)
     local self = setmetatable({}, Instance)
     self.hl = hl
     self.layout = layout
-    self.workspaceLayouts = {}
+    self.workspaceStates = {}
+
+    subscribleEvents(self)
 
     return self
 end
 
-
-function Instance:SaveWorkspaceLayout(workspaceID, layout)
-    self.workspaceLayouts[workspaceID] = layout
+function Instance:SaveWorkspaceState(workspace)
+    self.workspaceStates[workspace.id] = {
+        layout = self.hl.get_config("general.layout")
+    }
 end
 
-function Instance:SaveWorkspaceState()
-    local workspace = self.hl.get_active_workspace()
-    self:SaveWorkspaceLayout(workspace.id, self.hl.get_config("general.layout"))
+function Instance:LoadWorkspaceLayout(workspace)
+    local workspaceState = self.workspaceStates[workspace.id]
+
+    if workspaceState then
+        self.layout:ChangeLayout(workspaceState.layout)
+    end
 end
 
-function Instance:SwitchWorkspaceLayout(workspaceID)
-    self.layout:ChangeLayout(self.workspaceLayouts[workspaceID])
-end
+function Instance:SwitchToWorkspace(workspaceSelector)
+    local hl = self.hl
 
-function Instance:SwitchWorkspace(workspaceID)
-    self:SaveWorkspaceState()
-    self.hl.dispatch(self.hl.dsp.focus({workspace = workspaceID}))
+    self:SaveWorkspaceState(hl.get_active_workspace())
+    hl.dispatch(hl.dsp.focus({workspace = workspaceSelector}))
 
-    local workspace = self.hl.get_active_workspace()
+    local workspace = hl.get_active_workspace()
 
-    self:SwitchWorkspaceLayout(workspace.id)
+    self:LoadWorkspaceLayout(workspace)
+
+    return workspace
 end
 
 function Instance:NewWorkspace()
     local workspaces = self.hl.get_workspaces()
-    self.hl.notification.create({text = #workspaces, timeout = 1000})
-    self:SwitchWorkspace(#workspaces + 1)
+    local workspace = self:SwitchToWorkspace(workspaces[#workspaces].id + 1)
+    return workspace
+end
+
+function Instance:SwapWorkspaceMonitor()
+    local hl = self.hl
+
+    local monitors = hl.get_monitors()
+    local monitorsPerId = {}
+
+    local maxId = 0
+    for _, v in ipairs(monitors) do
+        monitorsPerId[v.id] = v
+        if v.id > maxId then
+            maxId = v.id
+        end
+    end
+
+    local currentMonitor = hl.get_active_window().monitor
+    local nextMonitorId = currentMonitor.id - 1
+    local nextMonitor = monitorsPerId[nextMonitorId]
+
+
+    if not nextMonitor then
+        local id = 0
+        while id <= maxId do
+            id = id +1
+            nextMonitor = monitorsPerId[id]
+            if nextMonitor then break end
+        end
+    end
+
+
+    hl.dispatch(hl.dsp.workspace.move({monitor = nextMonitor.name }))
 end
 
 function Instance:SwitchNextWorkspace()
-    self:SwitchWorkspace("e+1")
+    self:SwitchToWorkspace("e+1")
 end
 
 function Instance:SwitchPreviousWorkspace()
-    self:SwitchWorkspace("e-1")
+    self:SwitchToWorkspace("e-1")
 end
 
+function Instance:SwitchNextMonitorWorkspace()
+    self:SwitchToWorkspace("m+1")
+end
+
+function Instance:SwitchPreviousMonitorWorkspace()
+    self:SwitchToWorkspace("m-1")
+end
 
 return Instance
